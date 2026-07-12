9c76e9f086f242b05d9e33470bc89475541483f2
akranetendoi-web <akranetendoi@gmail.com>
2026-07-12 13:00:09 +0700

    Delete api directory

--- api/config.js
+++
@@ -1,7 +0,0 @@
-// File: api/config.js
-export default function handler(req, res) {
-    // Membaca Client Key dari Environment Vercel dan mengirimkannya ke Frontend
-    res.status(200).json({
-        midtrans_client_key: process.env.MIDTRANS_CLIENT_KEY || ""
-    });
-}
